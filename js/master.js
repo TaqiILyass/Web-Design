362fe91008e108605d8c86eeb36c9993530db836
TaqiIlyass <ilyasstaqi7@gmail.com>
2024-04-12 18:42:09 +0000

    The Final Modified

--- js/master.js
+++ js/master.js
@@ -195,3 +195,47 @@ scrollToSomewhere(allBullets)
 const allLinks = document.querySelectorAll(".links a")
 scrollToSomewhere(allLinks)
 
+
+// Toggle Menu
+let toggleBtn = document.querySelector('.toggel-menu');
+let tLinks = document.querySelector('.links');
+
+toggleBtn.onclick = function (e) {
+
+    // Stop Propagation
+    e.stopPropagation()
+    
+    // Toggle Class "menu-active" On Button
+    this.classList.toggle('menu-active')
+
+    // Toggle Class "open" On Links
+    tLinks.classList.toggle('open')
+
+}
+
+// Click Anywhere Outside Menu And Toggle Button
+document.addEventListener('click', (e) => {
+
+
+    if (e.target !== toggleBtn && e.target !== tLinks) {
+        
+        // Check If Menu Is Open
+        if (tLinks.classList.contains('open')) {
+            
+            // Toggle Class "menu-active" On Button
+            toggleBtn01.classList.toggle('menu-active')
+
+            // Toggle Class "open" On Links
+            tLinks.classList.toggle('open')
+
+        }
+
+    }
+
+
+})
+
+// // Stop Propagation On Menu
+tLinks.onclick = function(e) {
+    e.stopPropagation()
+}
